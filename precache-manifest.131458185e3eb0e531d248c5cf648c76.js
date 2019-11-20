591c17ed946ee530c4739fca91add8066f3b6fd1
Oleh Korshun <oleg.korshun@outlook.com>
2019-11-20 23:45:54 +0300

    Updates

---
+++ precache-manifest.131458185e3eb0e531d248c5cf648c76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac9e05b1e7cc48281d9e22372c5cd9f",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "5913b56c4826459b18e2",
+    "url": "/todo/static/css/main.6d4a1877.chunk.css"
+  },
+  {
+    "revision": "2c3cb51bc269adbd856e",
+    "url": "/todo/static/js/2.f7f15e34.chunk.js"
+  },
+  {
+    "revision": "5913b56c4826459b18e2",
+    "url": "/todo/static/js/main.0a3c2a7e.chunk.js"
+  },
+  {
+    "revision": "0dfeb895ffce801d1a97",
+    "url": "/todo/static/js/runtime-main.f2a5c796.js"
+  }
+]);
